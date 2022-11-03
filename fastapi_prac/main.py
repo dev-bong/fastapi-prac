79fe30feefa0e895699539a03dfe89676c89167d
dev-bong <qhddnjswo011@gmail.com>
2022-11-03 16:42:47 +0900

    ✨ svelte 프론트 서버와 연결 기능 & 포맷팅 도구 black 추가

--- fastapi_prac/main.py
+++ fastapi_prac/main.py
@@ -1,8 +1,23 @@
+import os
+
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+front_url = os.environ.get("EXTERNAL_FRONT_URL", "http://127.0.0.1:5173")
 
+origins = [
+    front_url,
+]
 
-@app.get("/")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/hello")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Test msg"}
