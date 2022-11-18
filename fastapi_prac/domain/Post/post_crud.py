30a530b75ef211092758321c049e4a444dd802e2
dev-bong <qhddnjswo011@gmail.com>
2022-11-18 13:33:40 +0900

    ✨ 게시글 상세 조회 API 구현

--- fastapi_prac/domain/Post/post_crud.py
+++ fastapi_prac/domain/Post/post_crud.py
@@ -4,3 +4,7 @@ from sqlalchemy.orm import Session
 def get_post_list(db: Session):
     post_list = db.query(Post).order_by(Post.create_date.desc()).all()
     return post_list
+
+def get_post(db: Session, post_id: int):
+    post = db.query(Post).get(post_id)
+    return post
--- fastapi_prac/domain/Post/post_router.py
+++ fastapi_prac/domain/Post/post_router.py
@@ -12,3 +12,8 @@ router = APIRouter(
 def post_list(db: Session = Depends(get_db)):
     _post_list = post_crud.get_post_list(db)
     return _post_list
+
+@router.get("/detail/{post_id}", response_model=post_schema.Post)
+def post_detail(post_id: int, db: Session = Depends(get_db)):
+    post = post_crud.get_post(db, post_id=post_id)
+    return post
